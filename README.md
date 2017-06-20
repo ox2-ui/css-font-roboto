0770e69c42eb51e3a4279ed4ec858fa6cb93ae7e
Lauricio Su <me@lauricio.com>
2017-06-20 16:43:16 +0700

    📦 upgrade package config v2

--- README.md
+++ README.md
@@ -1,23 +1,28 @@
+# @ox2/css-font-roboto
+![NPM version](https://img.shields.io/badge/npm-private-orange.svg?style=flat)
+<!-- ![NPM version](https://img.shields.io/npm/v/@ox2/css-font-roboto.svg?style=flat) -->
+
 ## Roboto Font v15
 
 This package contains common *Roboto* font [styles](https://github.com/ox2/css-font-roboto#bundled-font-styles) that are converted to base64-encoded data URI strings and embedded into a single css file.
 
-### Installation
+## Installation
 Install using [npm](http://npmjs.com):
-```
+```sh
 npm install @ox2/css-font-roboto --save
 ```
 Install using [yarn](http://yarnpkg.com):
-```
+```sh
 yarn add @ox2/css-font-roboto
 ```
-### Why
+
+## Why
 This is useful for prototyping as you can start using the font quickly by importing single css file to your project/playground and it does not require internet connection to use it once the package has been installed. Depending on your setup and use case this could be used in a production app as well, for example you could merge it with your current style sheet and included in Cordova app bundle for a more consistent font loading experience.
 
-### Usage
+## Usage
 You can use it in a webpack project by importing it `import '@ox2/css-font-roboto'` in your top level js file like `index.js` or in a `.storybook/config.js` if your using [React Storybook](https://github.com/storybooks/react-storybook).
 
-### Bundled font styles
+## Bundled font styles
 - Light 300
 - Normal 400
 - Normal 400 Italic
@@ -25,10 +30,10 @@ You can use it in a webpack project by importing it `import '@ox2/css-font-robot
 - Bold 700
 - Bold 700 Italic
 
-### Bundled font language subsets
+## Bundled font language subsets
 - Latin
 
-### Font update instructions
+## Font update instructions
 Note: There may only be a need to update it when there is a new **font** version released by Google Fonts.
 
 Current font version: **v15**
@@ -51,15 +56,20 @@ cd build && npm publish
 **TIP**: You can check if theres a new font version by running the `download` script and inspecting font urls `...gstatic.com/s/roboto/v15/Hgo13k...` in the output file or just opening the google font link in your browser and inspecting urls there.
 
 
-### Build your own package
+## Build your own package
 You can easily build your own package with other font family/styles variations by changing `FONT_URL` variable in `scripts/download.sh` to your custom one from https://fonts.google.com/ and then following the steps in **Update Instructions** section.
 
-### Font License
+## Font License
 Apache License, version 2.0
 
 More info: https://www.google.com/fonts/specimen/Roboto
 
 Repository: https://github.com/google/roboto
 
-### Package License
-MIT
+## Change Log
+Changes are tracked in the [CHANGELOG.md](https://github.com/ox2/css-font-roboto/blob/master/CHANGELOG.md)
+
+## Package License
+[MIT](https://github.com/ox2/css-font-roboto/blob/master/LICENSE)
+
+
--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -1,86 +1,97 @@
 /* eslint-disable no-console */
-import path from 'path';
 import fse from 'fs-extra';
+import path from 'path';
 
-const files = [
-  'README.md',
-  'LICENSE',
-];
+const files = ['README.md', 'CHANGELOG.md', 'LICENSE'];
+const space = 2;
 
-Promise.all(
-  files.map((file) => copyFile(file))
-)
-.then(() => createPackageFile());
+function resolveBuildPath(file) {
+  return path.resolve(
+    __dirname,
+    '../build/',
+    path.basename(file),
+  );
+}
 
 function copyFile(file) {
   const buildPath = resolveBuildPath(file);
-  return new Promise((resolve) => {
-    fse.copy(
-      file,
-      buildPath,
-      (err) => {
-        if (err) throw err;
-        resolve();
-      }
-    );
-  })
-  .then(() => console.log(`Copied ${file} to ${buildPath}`));
-}
-
-function resolveBuildPath(file) {
-  return path.resolve(__dirname, '../build/', path.basename(file));
+  return new Promise(resolve => {
+    fse.copy(file, buildPath, err => {
+      if (err) throw err;
+      resolve();
+    });
+  }).then(() =>
+    console.log(`Copied ${file} to ${buildPath}`),
+  );
 }
 
 function createPackageFile() {
-  return new Promise((resolve) => {
-    fse.readFile(path.resolve(__dirname, '../package.json'), 'utf8', (err, data) => {
-      if (err) {
-        throw err;
-      }
+  return new Promise(resolve => {
+    fse.readFile(
+      path.resolve(__dirname, '../package.json'),
+      'utf8',
+      (err, data) => {
+        if (err) {
+          throw err;
+        }
 
-      resolve(data);
-    });
+        resolve(data);
+      },
+    );
   })
-  .then((data) => JSON.parse(data))
-  .then((packageData) => {
-    const {
-      name,
-      author,
-      version,
-      description,
-      keywords,
-      repository,
-      license,
-      main,
-      bugs,
-      homepage,
-      peerDependencies,
-      dependencies,
-    } = packageData;
+    .then(data => JSON.parse(data))
+    .then(packageData => {
+      const {
+        name,
+        author,
+        version,
+        description,
+        keywords,
+        repository,
+        license,
+        main,
+        bugs,
+        homepage,
+        peerDependencies,
+        dependencies,
+      } = packageData;
 
-    const minimalPackage = {
-      name,
-      author,
-      version,
-      description,
-      keywords,
-      repository,
-      license,
-      main,
-      bugs,
-      homepage,
-      peerDependencies,
-      dependencies,
-    };
+      const minimalPackage = {
+        name,
+        author,
+        version,
+        description,
+        keywords,
+        repository,
+        license,
+        main,
+        bugs,
+        homepage,
+        peerDependencies,
+        dependencies,
+      };
 
-    return new Promise((resolve) => {
-      const buildPath = path.resolve(__dirname, '../build/package.json');
-      const data = JSON.stringify(minimalPackage, null, 2);
-      fse.writeFile(buildPath, data, (err) => {
-        if (err) throw (err);
-        console.log(`Created package.json in ${buildPath}`);
-        resolve();
+      return new Promise(resolve => {
+        const buildPath = path.resolve(
+          __dirname,
+          '../build/package.json',
+        );
+        const data = JSON.stringify(
+          minimalPackage,
+          null,
+          space,
+        );
+        fse.writeFile(buildPath, data, err => {
+          if (err) throw err;
+          console.log(
+            `Created package.json in ${buildPath}`,
+          );
+          resolve();
+        });
       });
     });
-  });
 }
+
+Promise.all(files.map(file => copyFile(file))).then(() =>
+  createPackageFile(),
+);
